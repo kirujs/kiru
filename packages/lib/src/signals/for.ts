ff27b0bc265dfc9b588537f4627d3cc0c12c456e
LankyMoose <rausten93@gmail.com>
2026-01-13 05:05:44 +1300

    lib - enable non-signal array usage with `<For />`

--- packages/lib/src/signals/for.ts
+++ packages/lib/src/signals/for.ts
@@ -1,25 +1,30 @@
 import type { Signal } from "./base.js"
+import { unwrap } from "./utils"
 
-type InferArraySignalItemType<T extends Signal<any[]>> = T extends Signal<
-  infer V
->
-  ? V extends Array<infer W>
-    ? W
+type InferArraySignalItemType<T extends Signal<any[]> | readonly unknown[]> =
+  T extends Signal<infer V>
+    ? V extends Array<infer W>
+      ? W
+      : never
+    : T extends unknown[]
+    ? T[number]
     : never
-  : never
 
-type ForProps<T extends Signal<any[]>, U = InferArraySignalItemType<T>> = {
+type ForProps<
+  T extends Signal<any[]> | readonly unknown[],
+  U = InferArraySignalItemType<T>
+> = {
   each: T
   fallback?: JSX.Element
   children: (value: U, index: number, array: U[]) => JSX.Element
 }
 
-export function For<T extends Signal<any[]>>({
+export function For<T extends Signal<any[]> | unknown[]>({
   each,
   fallback,
   children,
 }: ForProps<T>) {
-  const items = each.value
+  const items = unwrap(each, true)
   if (items.length === 0) return fallback
   return items.map(children)
 }
