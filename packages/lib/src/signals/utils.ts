557da8cc49261b57ceb77197c5b5855900f12c52
LankyMoose <rausten93@gmail.com>
2026-02-25 06:27:17 +1300

    lib/signals - implement `untrack()`

--- packages/lib/src/signals/utils.ts
+++ packages/lib/src/signals/utils.ts
@@ -1,13 +1,20 @@
 import { call } from "../utils/index.js"
 import { Signal } from "./base.js"
-import { effectQueue } from "./globals.js"
+import { effectQueue, tracking } from "./globals.js"
 
 export function unwrap<T>(value: T | Signal<T>, reactive = false): T {
   if (!Signal.isSignal(value)) return value
   return reactive ? value.value : value.peek()
 }
 
-export const tick = () => {
+export function tick() {
   effectQueue.forEach(call)
   effectQueue.clear()
+}
+
+export function untrack<T>(fn: () => T) {
+  tracking.enabled = false
+  const result = fn()
+  tracking.enabled = true
+  return result
 }
