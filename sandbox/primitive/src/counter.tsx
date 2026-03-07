19ff411ed0a4bf8c40e8b761e48b44910c1c30a5
LankyMoose <rausten93@gmail.com>
2026-03-07 15:05:01 +1300

    lib - remove unused `exports` entries in pkg json

--- sandbox/primitive/src/counter.tsx
+++ sandbox/primitive/src/counter.tsx
@@ -1,4 +1,13 @@
-import { signal } from "kiru"
+import { computed, signal } from "kiru"
+
+const x = signal(0)
+const y = computed(() => {
+  console.log("run y")
+  return x.value * 2
+})
+debugger
+
+y.subscribe((v) => console.log(v))
 
 export const Counter = () => {
   const count = signal(0)
