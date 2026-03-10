1480abee014ddbe82c51164c6c614d82d01d49d5
LankyMoose <rausten93@gmail.com>
2026-03-11 04:05:05 +1300

    update sandbox/ssr

--- sandbox/csr/src/pages/index.tsx
+++ sandbox/csr/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { signal, resource, ErrorBoundary, Derive, computed, setup } from "kiru"
+import { signal, computed, resource, Derive } from "kiru"
 
 interface User {
   id: number
@@ -7,9 +7,8 @@ interface User {
   age: number
 }
 
-//const userId = signal(1)
-const limit = signal(4)
 const search = signal("")
+const limit = signal(10)
 const searchParams = computed(() => {
   return {
     search: search.value,
@@ -17,35 +16,31 @@ const searchParams = computed(() => {
   }
 })
 
+// able to be created globally _or_ within components
 const users = resource(searchParams, async (src, ctx) => {
   const res = await fetch(
     `https://dummyjson.com/users/search?q=${src.search}&limit=${src.limit}`,
     { signal: ctx.signal }
   )
-  if (Math.random() > 0.5) throw new Error("Network error")
   return res.json() as Promise<{ users: User[] }>
 })
 
 export default function HomePage() {
-  return () => (
+  return (
     <>
-      <ErrorBoundary fallback={(e) => <div>Error: {e.message}</div>}>
-        <Derive from={users} fallback={<div>Loading...</div>}>
-          {({ users }, isStale) => (
-            <div style={{ opacity: isStale ? 0.5 : 1 }}>
-              <ul>
-                {users.map((user) => (
-                  <li key={user.id}>
-                    {user.firstName} {user.lastName}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </Derive>
-      </ErrorBoundary>
-      errSig: {users.error}
-      <div>pending: {users.isPending.value.toString()}</div>
+      <Derive from={users} fallback={<div>Loading...</div>}>
+        {(data, isStale) => (
+          <div style={{ opacity: isStale ? 0.5 : 1 }}>
+            <ul>
+              {data.users.map((user) => (
+                <li key={user.id}>
+                  {user.firstName} {user.lastName}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Derive>
       <button onclick={() => users.refetch()}>Refetch</button>
       <input placeholder={"search"} bind:value={search} />
       <input
--- sandbox/ssr/src/pages/products/+Page.tsx
+++ sandbox/ssr/src/pages/products/+Page.tsx
@@ -1,4 +1,4 @@
-import { Derive, statefulPromise } from "kiru"
+import { Derive, resource, signal } from "kiru"
 
 interface ProductsResponse {
   products: {
@@ -17,22 +17,32 @@ interface ProductsResponse {
 }
 
 export function Page() {
-  const products = statefulPromise<ProductsResponse>(async (signal) => {
-    await new Promise((resolve) => setTimeout(resolve, 500))
-    const response = await fetch("https://dummyjson.com/products", { signal })
-    if (!response.ok) throw new Error(response.statusText)
-    return await response.json()
-  })
+  const limit = signal(10)
+  const products = resource<ProductsResponse, number>(
+    limit,
+    async (limit, { signal }) => {
+      await new Promise((resolve) => setTimeout(resolve, 500))
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=${limit}`,
+        { signal }
+      )
+      if (!response.ok) throw new Error(response.statusText)
+      return await response.json()
+    }
+  )
 
   return () => (
-    <Derive from={products} fallback={<div>Loading...</div>}>
-      {(data, isStale) => (
-        <ul className={isStale ? "opacity-50" : ""}>
-          {data.products.map((product) => (
-            <li key={product.id}>{product.title}</li>
-          ))}
-        </ul>
-      )}
-    </Derive>
+    <>
+      <input type="range" min={1} max={50} bind:value={limit} />
+      <Derive from={products} fallback={<div>Loading...</div>}>
+        {(data, isStale) => (
+          <ul className={isStale ? "opacity-50" : ""}>
+            {data.products.map((product) => (
+              <li key={product.id}>{product.title}</li>
+            ))}
+          </ul>
+        )}
+      </Derive>
+    </>
   )
 }
