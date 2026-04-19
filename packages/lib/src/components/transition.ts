22d2e37bcdd5ab65d299fc238d1027dbf9b510b1
LankyMoose <rausten93@gmail.com>
2026-04-19 20:49:39 +1200

    lib/components - simplify `<Transition>` impl

--- packages/lib/src/components/transition.ts
+++ packages/lib/src/components/transition.ts
@@ -1,10 +1,11 @@
 import { onMount } from "../hooks/onMount.js"
 import { unwrap } from "../signals/utils.js"
 import { setup } from "../hooks/setup.js"
+import type { Signalable } from "../types.js"
 
 export type TransitionState = "entering" | "entered" | "exiting" | "exited"
 interface TransitionProps {
-  in: boolean | Kiru.Signal<boolean>
+  in: Signalable<boolean>
   /**
    * Initial state of the transition
    * @default "exited"
@@ -21,29 +22,27 @@ interface TransitionProps {
 }
 
 /**
- * Animates the DOM in a procedural/coroutine-like fashion. Useful for modals, drawers, dialogs and more.
+ * Enables control of the DOM in a procedural/coroutine-like fashion. Useful for modals, drawers, dialogs and more.
  * @see https://kirujs.dev/docs/components/transition
  */
-export const Transition: Kiru.FC<TransitionProps> = (props) => {
+export const Transition: Kiru.FC<TransitionProps> = () => {
   const $ = setup<typeof Transition>()
   const tState = $.derive<TransitionState>((p) => p.initialState || "exited")
   const inState = $.derive((p) => unwrap(p.in, true))
   let timeoutRef: number | undefined
-  let onTransitionEnd = props.onTransitionEnd
-  let duration = props.duration
 
   const setTransitionState = (transitionState: TransitionState) => {
     clearTimeout(timeoutRef)
     tState.value = transitionState
     if (transitionState === "entered" || transitionState === "exited") {
-      onTransitionEnd?.(transitionState)
+      $.props.onTransitionEnd?.(transitionState)
     }
   }
 
   const queueStateChange = (transitionState: "entered" | "exited") => {
     timeoutRef = window.setTimeout(
       () => setTransitionState(transitionState),
-      getTiming(transitionState, duration)
+      getTiming(transitionState, $.props.duration)
     )
   }
 
@@ -68,11 +67,7 @@ export const Transition: Kiru.FC<TransitionProps> = (props) => {
     }
   })
 
-  return (props) => {
-    duration = props.duration
-    onTransitionEnd = props.onTransitionEnd
-    return props.element(tState.value)
-  }
+  return (props) => props.element(tState.value)
 }
 
 const defaultDuration = 150
