6ea7b2dac20399e9ff2fe40e534409bb26946445
LankyMoose <rausten93@gmail.com>
2026-05-16 07:59:41 +1200

    lib - make vNode IDs 50% smaller (no need to include depth for uniqueness)

--- packages/lib/src/utils/vdom.ts
+++ packages/lib/src/utils/vdom.ts
@@ -198,7 +198,6 @@ function createVNodeId(vNode: Kiru.VNode): string {
   let n: Kiru.VNode | null = vNode
   while (n) {
     accumulator.push(n.index)
-    accumulator.push(n.depth)
     n = n.parent
   }
   return `k:${BigInt(accumulator.join("")).toString(36)}`
