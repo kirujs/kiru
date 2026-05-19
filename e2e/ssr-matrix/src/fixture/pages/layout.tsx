90ae845be19a461fc05dee0114eb91dde124cb11
LankyMoose <rausten93@gmail.com>
2026-05-20 08:35:07 +1200

    fix incorrect 'children' type in e2e/ssr-matrix

--- e2e/ssr-matrix/src/fixture/pages/layout.tsx
+++ e2e/ssr-matrix/src/fixture/pages/layout.tsx
@@ -1,4 +1,4 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: JSX.Children }) {
   return (
     <main>
       <nav>
